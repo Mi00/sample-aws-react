1f0b900de3406cc14335f82385337fad1e504c14
michalkaliniewicz <michal.kaliniewicz@siili.com>
2023-02-13 20:38:30 +0100

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Authenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user && user.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
+    );
 }
-
-export default App;
